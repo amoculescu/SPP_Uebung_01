f5a17c17e2f2f24f741ee7d334732a201c501936
JohBeck <johannes_beck@arcor.de>
2018-11-25 16:48:21 +0100

    improvements or task 1

--- Praktikum1/hello-openmp.c
+++ Praktikum1/hello-openmp.c
@@ -5,7 +5,7 @@ int main ( int argc, char *argv[] ) {
 	// Current output may differ from the one asked -> CHECK AGAIN! 
     printf("Hello parallel world!.\n");
     printf("Currently there can be up to %d threads being used.\n", omp_get_max_threads());
-    omp_set_num_threads(omp_get_max_threads()/2); //Change number of used-threads here!
+    omp_set_num_threads(3); //Change number of used-threads here!
     printf("But we will only use %d\n", omp_get_max_threads());
 #pragma omp parallel
     {
