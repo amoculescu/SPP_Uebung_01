2bb1ddf0b3ce3e2eedaf0a30cf52e5ae99157dde
louisdreher <dreher.louis@googlemail.com>
2019-01-09 16:30:23 +0100

    task2

--- Praktikum2/task2.c
+++ Praktikum2/task2.c
@@ -17,75 +17,120 @@
 /**
  * Checks whether arr is sorted locally
  **/
-int is_arr_sorted( int* arr, int len ) {
+int is_arr_sorted(int* arr, int len) {
 
-    int i;
-    for( i = 0; i < len - 1; ++i )
-        if( arr[ i ] > arr[ i + 1 ] )
-            return 0;
-    return 1;
+	int i;
+	for (i = 0; i < len - 1; ++i)
+		if (arr[i] > arr[i + 1])
+			return 0;
+	return 1;
 }
 
 
 /**
- * Checks whether arr is sorted globally. 
+ * Checks whether arr is sorted globally.
  **/
-int verify_results( int* arr, int len, int myrank, int nprocs ) {
+int verify_results(int* arr, int len, int myrank, int nprocs) {
 
-    int is_sorted_global = 0;
+	int is_sorted_global = 0;
 
-    // TODO
+	// TODO
 
-    return is_sorted_global;
+	return is_sorted_global;
 }
 
 
 /**
  * This function compares two integers.
  */
-int comp_func( const void* a, const void* b ) {
-    return ( *(int*)a - *(int*)b );
+int comp_func(const void* a, const void* b) {
+	return (*(int*)a - *(int*)b);
 }
 
 
 /**
  * Returns unique random integer.
  */
-int get_unique_rand_elem( unsigned int total_count_bits, unsigned int index, double scale ) {
+int get_unique_rand_elem(unsigned int total_count_bits, unsigned int index, double scale) {
 
-    int random_elem = (int)( scale * drand48() );
-    int unique_random_element = ( random_elem << total_count_bits ) | index;
+	int random_elem = (int)(scale * drand48());
+	int unique_random_element = (random_elem << total_count_bits) | index;
 
-    return unique_random_element;
+	return unique_random_element;
 }
 
 
 /**
  * Merges arr_from into arr.
  */
-void merge_arr( int* arr1, int len1, int* arr2, int len2, int* arr_out, int* len_out ) {
+void merge_arr(int* arr1, int len1, int* arr2, int len2, int* arr_out, int* len_out) {
 
-    int idx1 = 0, idx2 = 0, idx3 = 0;
-    while( idx1 < len1 ) {
-        while( idx2 < len2 && 
-               arr2[ idx2 ] < arr1[ idx1 ] ) {
-            arr_out[ idx3++ ] = arr2[ idx2++ ];
-        }
-        arr_out[ idx3++ ] = arr1[ idx1++ ];
-    }
-    while( idx2 < len2 ) {
-        arr_out[ idx3++ ] = arr2[ idx2++ ];
-    }
-    *len_out = idx3;
+	int idx1 = 0, idx2 = 0, idx3 = 0;
+	while (idx1 < len1) {
+		while (idx2 < len2 &&
+			arr2[idx2] < arr1[idx1]) {
+			arr_out[idx3++] = arr2[idx2++];
+		}
+		arr_out[idx3++] = arr1[idx1++];
+	}
+	while (idx2 < len2) {
+		arr_out[idx3++] = arr2[idx2++];
+	}
+	*len_out = idx3;
 }
 
 
 /**
  * All-gather-merge using hypercube approach.
  */
-void all_gather_merge( int* arr, int len, int** out_arr, int* out_len, 
-                       int nprocs, MPI_Comm comm ) {
-    // TODO
+void all_gather_merge(int* arr, int len, int** out_arr, int* out_len,
+	int nprocs, MPI_Comm comm) {
+
+	// get own rank
+	int myrank;
+	MPI_Comm_rank(comm, &myrank);
+
+	//communication partner parameters
+	int kp_rank;
+	int kp_array_size;
+	int* kp_array = NULL;
+
+	//array for merging
+	int* merged_array = NULL;;
+
+	MPI_Status status;
+
+	for (int i = 0; i < nprocs; i = i * 2) {
+		kp_rank = myrank ^ i;
+
+		// exchange arrays size
+		MPI_Sendrecv(&len, 1, MPI_INT, kp_rank, 0, &kp_array_size, 1, MPI_INT, kp_rank, 0, &status);
+
+		//create output arrays
+		kp_array = malloc(sizeof(int) * kp_array_size);
+		merged_array = malloc(sizeof(int) * (kp_array_size + len));
+
+		// exchange arrays
+		MPI_Sendrecv(arr, len, MPI_INT, kp_rank, 0, kp_array, kp_array_size, MPI_INT, kp_rank, 0, &status);
+
+		// merge array output: merged_array..  len =  new lenght
+		merge_arr(arr, len, kp_array, kp_array_size, merged_array, &len);
+
+		//free old arrays
+		free(arr);
+		free(kp_array);
+
+		// set the pointer arr to point at the same adress as merged_array; 
+		arr = merged_array;
+	}
+	// arr and merged_array point both point to the finel array
+	// len is th length of the meged array
+
+	// setze den pointer auf den **out_arr zeigt auf die adresse von merged_array
+	*out_arr = merged_array;
+	*out_len = len;
+
+	// TODO
 }
 
 
@@ -93,117 +138,150 @@ void all_gather_merge( int* arr, int len, int** out_arr, int* out_len,
  * Initilizes the input. Each process will have a random local array of numbers. The
  * length of this array is anywhere between 0 to MAX_NUM_LOCAL_ELEMS
  */
-void init_input( int w_myrank, int w_nprocs, int* input_arr, 
-                 int* input_len, int* total_elems ) {
+void init_input(int w_myrank, int w_nprocs, int* input_arr,
+	int* input_len, int* total_elems) {
 
-    // Initialize random seed
-    srand48( w_nprocs );
+	// Initialize random seed
+	srand48(w_nprocs);
 
-    // Total number of elements is 65% of the number of processes
-    *total_elems = (int)( w_nprocs * 0.65 );
-    int* global_arr = NULL;
-    int* sendcnts = NULL;
-    int* displs = NULL;
+	// Total number of elements is 65% of the number of processes
+	*total_elems = (int)(w_nprocs * 0.65);
+	int* global_arr = NULL;
+	int* sendcnts = NULL;
+	int* displs = NULL;
 
-    if( w_myrank == 0 ) {
-        printf( "Total number of input elements: %d\n", *total_elems );
+	if (w_myrank == 0) {
+		printf("Total number of input elements: %d\n", *total_elems);
 
-        global_arr = malloc( *total_elems * sizeof(int) );
+		global_arr = malloc(*total_elems * sizeof(int));
 
-        double scale = *total_elems * 5;
-        int total_count_bits = (int)ceil( log( *total_elems ) / log( 2.0 ) );
+		double scale = *total_elems * 5;
+		int total_count_bits = (int)ceil(log(*total_elems) / log(2.0));
 
-        // Init global array with random elements
-        for( int i = 0; i < *total_elems; ++i )
-            global_arr[i] = get_unique_rand_elem( total_count_bits, i, scale );
+		// Init global array with random elements
+		for (int i = 0; i < *total_elems; ++i)
+			global_arr[i] = get_unique_rand_elem(total_count_bits, i, scale);
 
-        // Randomly decide how much elements each rank will get
-        sendcnts = malloc( w_nprocs * sizeof(int) );
-        memset( sendcnts, 0, w_nprocs * sizeof(int) );
-        int total_elem_cnt = *total_elems;
-        for( int i = 0; i < w_nprocs; ++i ) {
-            double coin_flip = drand48();
-            if( coin_flip < 0.45 ) {
-                sendcnts[i]++;
-                total_elem_cnt--;
-                if( total_elem_cnt == 0 ) break;
-                coin_flip = drand48();
-                if( coin_flip < 0.35 ) {
-                    sendcnts[i]++;
-                    total_elem_cnt--;
-                    if( total_elem_cnt == 0 ) break;
-                    if( coin_flip < 0.15 ) {
-                        sendcnts[i]++;
-                        total_elem_cnt--;
-                        if( total_elem_cnt == 0 ) break;
-                    }
-                }
-            }
-        }
+		// Randomly decide how much elements each rank will get
+		sendcnts = malloc(w_nprocs * sizeof(int));
+		memset(sendcnts, 0, w_nprocs * sizeof(int));
+		int total_elem_cnt = *total_elems;
+		for (int i = 0; i < w_nprocs; ++i) {
+			double coin_flip = drand48();
+			if (coin_flip < 0.45) {
+				sendcnts[i]++;
+				total_elem_cnt--;
+				if (total_elem_cnt == 0) break;
+				coin_flip = drand48();
+				if (coin_flip < 0.35) {
+					sendcnts[i]++;
+					total_elem_cnt--;
+					if (total_elem_cnt == 0) break;
+					if (coin_flip < 0.15) {
+						sendcnts[i]++;
+						total_elem_cnt--;
+						if (total_elem_cnt == 0) break;
+					}
+				}
+			}
+		}
 
-        // Redistribute remaining counts
-        int curr_rank = 0;
-        while( total_elem_cnt > 0 ) {
-            while( sendcnts[curr_rank] >= MAX_NUM_LOCAL_ELEMS )
-                ++curr_rank;
-            sendcnts[curr_rank]++;
-            total_elem_cnt--;
-        }
+		// Redistribute remaining counts
+		int curr_rank = 0;
+		while (total_elem_cnt > 0) {
+			while (sendcnts[curr_rank] >= MAX_NUM_LOCAL_ELEMS)
+				++curr_rank;
+			sendcnts[curr_rank]++;
+			total_elem_cnt--;
+		}
 
-        displs = malloc( w_nprocs * sizeof(int) );
-        displs[0] = 0;
-        for( int i = 1; i < w_nprocs; ++i )
-            displs[i] = displs[i - 1] + sendcnts[i - 1];
-    }
+		displs = malloc(w_nprocs * sizeof(int));
+		displs[0] = 0;
+		for (int i = 1; i < w_nprocs; ++i)
+			displs[i] = displs[i - 1] + sendcnts[i - 1];
+	}
 
-    // Redistribute the input length
-    MPI_Scatter( sendcnts, 1, MPI_INT, input_len, 1, MPI_INT, 0, MPI_COMM_WORLD );
+	// Redistribute the input length
+	MPI_Scatter(sendcnts, 1, MPI_INT, input_len, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Redistribute the input
-    MPI_Scatterv( global_arr, sendcnts, displs, MPI_INT, input_arr, *input_len, MPI_INT, 
-                  0, MPI_COMM_WORLD );
+	// Redistribute the input
+	MPI_Scatterv(global_arr, sendcnts, displs, MPI_INT, input_arr, *input_len, MPI_INT,
+		0, MPI_COMM_WORLD);
 
-    free( global_arr );
-    free( sendcnts );
-    free( displs );
+	free(global_arr);
+	free(sendcnts);
+	free(displs);
 }
 
-int main( int argc, char** argv ) {
+int main(int argc, char** argv) {
 
-    int w_myrank, w_nprocs;
-    MPI_Init( &argc, &argv );
-    MPI_Comm_rank( MPI_COMM_WORLD, &w_myrank );
-    MPI_Comm_size( MPI_COMM_WORLD, &w_nprocs );
+	int w_myrank, w_nprocs;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &w_myrank);
+	MPI_Comm_size(MPI_COMM_WORLD, &w_nprocs);
 
-    //
-    // Initialization phase
-    //
-    int n = 0;
-    int total_n;
-    int elem_arr[MAX_NUM_LOCAL_ELEMS];
-    
-    init_input( w_myrank, w_nprocs, elem_arr, &n, &total_n );
+	//
+	// Initialization phase
+	//
+	int n = 0;
+	int total_n;
+	int elem_arr[MAX_NUM_LOCAL_ELEMS];
 
-    
-    //
-    // TODO
-    //
+	init_input(w_myrank, w_nprocs, elem_arr, &n, &total_n);
 
 
-    //
-    // Verify the data is sorted globally
-    //
-    int res = verify_results( elem_arr, n, w_myrank, w_nprocs );
-    if( w_myrank == 0 ) {
-        if( res ) {
-            printf( "Results correct!\n" );
-        }
-        else {
-            printf( "Results incorrect!\n" );
-        }
-    }
- 
-    MPI_Finalize();
+	//
+	// TODO
+	//
 
-    return 0;
+	// split rows
+	int color_rows = w_myrank / sqrt(w_nprocs);
+	MPI_Comm row_comm;
+	MPI_Comm_split(MPI_COMM_WORLD, color_rows, w_myrank, &row_comm);
+
+	// set rank and number of procs in row
+	int r_myrank, r_nprocs;
+	MPI_Comm_rank(row_comm, &r_myrank);
+	MPI_Comm_size(row_comm, &r_nprocs);
+
+	//split columns
+	int color_cols = r_myrank;
+	MPI_Comm col_comm;
+	MPI_Comm_split(MPI_COMM_WORLD, color_cols, r_myrank, &col_comm);
+
+	//set rank and number of procs in column
+	int c_myrank, c_nprocs;
+	MPI_Comm_rank(col_comm, &c_myrank);
+	MPI_Comm_size(col_comm, &c_nprocs);
+
+	qsort(elem_arr, MAX_NUM_LOCAL_ELEMS, sizeof(int), comp_func);
+
+	int* merged_array_row;
+	int merged_array_row_length;
+
+	all_gather_merge(elem_arr, n, &merged_array_row, &merged_array_row_length, r_nprocs, row_comm);
+
+	int* merged_array_col;
+	int merged_array_col_length;
+
+	all_gather_merge(elem_arr, n, &merged_array_col, &merged_array_col_length, c_nprocs, col_comm);
+
+
+
+	//
+	// Verify the data is sorted globally
+	//
+	int res = verify_results(elem_arr, n, w_myrank, w_nprocs);
+	if (w_myrank == 0) {
+		if (res) {
+			printf("Results correct!\n");
+		}
+		else {
+			printf("Results incorrect!\n");
+		}
+	}
+
+	MPI_Finalize();
+
+	return 0;
 }
